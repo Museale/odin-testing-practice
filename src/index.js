a0b712ffd827ecd231ddede09847df6678a11f1d
elat <elat.webdev@gmail.com>
2023-11-22 16:01:21 +0100

    calculator func add, test for addition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { capitalize } from "./capitalize";
 import { sum } from "./sum";
 import { reverseString } from "./reverseString";
+import { calculate } from "./calculator";
 
 
 console.log(capitalize('hi, there'));
 console.log(sum(1,2));
 console.log(reverseString('Reversed'));
+console.log(calculate.add(1,2));
--- src/jest.test.js
+++ src/jest.test.js
@@ -1,7 +1,7 @@
  import { sum } from "./sum.js";
  import { capitalize } from "./capitalize.js";
  import { reverseString } from "./reverseString.js";
-import expect from "expect";
+ import { calculate } from "./calculator.js";
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
@@ -15,5 +15,10 @@ test('First letter is capital', () => {
 test('Reverse the string', () => {
     const string = 'reversed';
     const reverseTheString = reverseString(string);
-    expect(reverseTheString).toBe('desrever')
+    expect(reverseTheString).toBe('desrever');
+})
+
+
+test('1 + 2 equals 3', () => {
+    expect(calculate.add(1,2)).toBe(3);
 })
